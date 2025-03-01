b6c7cb6710b1115e1a34d90b68b3a7baaa10cfc4
Jack Maunsell <jackmaun@colostate.edu>
2025-02-28 21:45:46 -0700

    icky code

--- irc/src/gen-cert.bat
+++ irc/src/gen-cert.bat
@@ -1,11 +1,11 @@
 @echo off
 set OPENSSL_PATH="C:\Program Files\OpenSSL-Win64\bin\openssl.exe"
 
-echo Generating private key...
+echo generating private key...
 %OPENSSL_PATH% genrsa -out server.key 2048
 
-echo Generating certificate...
+echo generating certificate...
 %OPENSSL_PATH% req -x509 -new -nodes -key server.key -sha256 -days 1024 -out server.crt
 
-echo Done!
+echo done
 pause
